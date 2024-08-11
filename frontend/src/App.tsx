f079c5a6a10884a476a12fa1fd1cd96a872f2574
Pushpender <pushpenderrajputsp@gmail.com>
2024-08-11 23:02:40 +0530

    signup at Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/"  element={<Signup />} />
           <Route path="/signup"  element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
