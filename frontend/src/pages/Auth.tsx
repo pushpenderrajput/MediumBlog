378b85897c3b3048bd629a3086df1203c63ebc3f
Pushpender Rajput <85430197+pushpenderrajput@users.noreply.github.com>
2025-03-18 18:43:26 +0530

    Update Auth.tsx

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -56,7 +56,7 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
                         })
                     }} />
                     <button onClick={sendRequest} type="button" className="w-full mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                        {type === "signup" ? "Sign up" : "Sign in"}
+                        {type === "signup" ? "Sign in" : "Sign up"}
                     </button>
                     {/* {JSON.stringify(postInputs)} */}
                 </div>
